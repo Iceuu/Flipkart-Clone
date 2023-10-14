5f2f24d54e14b74c43aa1a8d68a28a840bef1340
iceuu <iswaryagunasekeran77@gmail.com>
2023-10-14 20:09:36 +0530

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
 
     return (
 
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Navbar />
             <Routes>
                 <Route path='/' element={<Home />} />
